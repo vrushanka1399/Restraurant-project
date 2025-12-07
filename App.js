899b23ac7d1af297c8463905d14f0cbe1d48367f
vrushanka1399 <vrushanka.1si17cs132@gmail.com>
2025-12-07 10:57:27 +0530

    Add files via upload
    Beginner Restraurant project

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/layout/Header';
+import React, { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+    <Header/>
+    </Fragment>
+
+  );
+}
+
+export default App;
