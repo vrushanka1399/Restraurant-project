8bfc71022f2578dea4e58cbedde70e13c947c89a
vrushanka1399 <vrushanka.1si17cs132@gmail.com>
2025-12-10 11:45:07 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/layout/Header';
-import React, { Fragment } from "react";
+import { Fragment, useState } from "react";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+    const [cartIsShown, setCartIsShown]=useState(false);
+
+    const showCartHandler=()=>{
+        setCartIsShown(true);
+    };
+
+    const hideCartHandler=()=>{
+        setCartIsShown(false);
+    };
+
+
   return (
     <Fragment>
-    <Header/>
+    {cartIsShown && <Cart onClose={hideCartHandler}/>}
+    <Header onShowCart={showCartHandler}/>
+    <main>
+    <Meals/>
+    </main>
     </Fragment>
 
   );
--- Header.js
+++ Header.js
@@ -1,18 +1,18 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 
-const Header=props=>{
+const Header=(props)=>{
 	return(
 	<Fragment>
 	<header className={classes.header}>
 	<h1>ReactMeals</h1>
-	<button>Cart</button>
+	<HeaderCartButton onClick={props.onShowCart}/>
 	</header>
 	<div className={classes['main-image']}>
 	<img src={mealsImage} alt='A table full of delicious food!' />
 	</div>
-	
 	</Fragment>
 	);
 };
